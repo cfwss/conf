f38de7be88f3d7444d2d2ee943d3da8d40a905d8
Aeakaw <yhmxb@vip.qq.com>
2023-11-19 13:02:39 +0800

    tls.sh

---
+++ agent/Auto/tls.sh
@@ -0,0 +1,11 @@
+cat << EOF > /etc/tls/test.crt
+-----BEGIN CERTIFICATE-----
+证书内容
+-----END CERTIFICATE-----
+EOF
+cat << EOF > /etc/tls/test.key
+-----BEGIN PRIVATE KEY-----
+私钥内容
+-----END PRIVATE KEY-----
+EOF
+
