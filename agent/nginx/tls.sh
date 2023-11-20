d18c16acafc1235cfa00cf7b5037e59017a5ef5b
Aeakaw <yhmxb@vip.qq.com>
2023-11-21 01:28:43 +0800

    tls.sh

---
+++ agent/nginx/tls.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+count=0
+
+OUTPUT_FILE="/tmp/tls_certificates_info.txt"
+echo "TLS证书信息：" > $OUTPUT_FILE
+echo "数量, 名称, 路径, 域名" >> $OUTPUT_FILE
+
+JSON_FILE="/etc/v2ray-agent/xray/conf/02_VLESS_TCP_inbounds.json"
+CERT_DIR="/etc/tls"
+num=10
+for cert in "$CERT_DIR"/*.crt; do
+ 
+
+        jq --argjson newEntry "$new_entry" '.inbounds[0].streamSettings.tlsSettings.certificates += [$newEntry]' "$JSON_FILE" > temp.json && mv temp.json "$JSON_FILE"
+    fi
+done
+
+
+
+sudo systemctl restart nginx
