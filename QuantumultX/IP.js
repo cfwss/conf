5af058e935d717fcd41189bbd8ddde3f3c13f1aa
Aeakaw <15069203+Aeakaw@users.noreply.github.com>
2023-12-06 19:27:36 +0800

    Update IP.js

--- QuantumultX/IP.js
+++ QuantumultX/IP.js
@@ -4,9 +4,3 @@ if ($response.statusCode !== 200) {
 
 
 
-var body = $response.body;
-var obj = JSON.parse(body);
-
-var title = flags.get(obj['countryCode']) + ' ' + City_ValidCheck(obj['city']);
-var subtitle = ISP_ValidCheck(obj['org']);
-var ip = obj['query'];
