42ea3dec6a4b9846f343822e8110ae163a12477b
Aeakaw <15069203+Aeakaw@users.noreply.github.com>
2024-01-20 01:39:14 +0800

    Delete install/Manual/Powershell_get_base64.ps1

--- install/Manual/Powershell_get_base64.ps1
+++
@@ -1,8 +0,0 @@
-$uuid = Read-Host "请输入 UUID"
-$base64_uuid = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($uuid)) -replace '[/+]'
-$base64_uuid = $base64_uuid.Substring(0, 22)
-while ($base64_uuid.Length % 4 -ne 0) {
-    $base64_uuid += '='
-}
-Write-Host "UUID: $uuid"
-Write-Host "BASE64: $base64_uuid"
